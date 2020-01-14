1fde8bdf2c1a6a563dd07041f705cc3cdd4a6a23
Cabroplatin <lisowski.mateusz90@gmail.com>
2020-01-14 21:03:22 +0000

    tried adding results lisitng funcionality but could only log them to the console

--- app.js
+++ app.js
@@ -1,28 +1,46 @@
 let userChosen
 let computerChosen
 var result = results()
+// var resultsLog = resultsList
 const displayResult = document.getElementById('result')
 const computerChoice = document.getElementById('computer-choice')
-const randomNumber = Math.round(Math.random()*(3))
+const randomNumber = Math.round(Math.random() * (3))
 const userChoice = document.getElementById('user-choice')
 const possibleChoices = document.querySelectorAll('.choices')
+const taskList = document.querySelector('.collection')
+const resultsList = document.querySelector('#result')
+
+
 
 // user choice
 
 possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('click', (e) => {
-    userChosen = e.target.id 
+    userChosen = e.target.id
     generatedComputerChoice()
     results()
     userChoice.innerHTML = userChosen
     computerChoice.innerHTML = computerChosen
     displayResult.innerHTML = result
+    console.log(resultsList)
+    resultsList.addEventListener('result', addResult)
+    
 }))
+
+function addResult(e) {
+    const li = document.createElement('li');
+    // li.className = 'result';
+    li.appendChild(document.createTextNode(resultsList.value));
+    resultsList.appendChild(li);
+    e.preventDefault();
+    
+}
+
 
 //random comp gen choice
 
 function generatedComputerChoice() {
     if (randomNumber === 1) {
-        return computerChosen ='rock'
+        return computerChosen = 'rock'
     } else if (randomNumber === 2) {
         return computerChosen = 'paper'
     } else if (randomNumber === 3) {
@@ -35,17 +53,22 @@ function generatedComputerChoice() {
 function results() {
     if (computerChosen === userChosen) {
         return result = 'Tie!'
-    } else if ( computerChosen === 'rock' && userChosen === 'paper') {
+    } else if (computerChosen === 'rock' && userChosen === 'paper') {
         return result = 'You Win!'
-    } else if ( computerChosen === 'rock' && userChosen === 'scissors') {
-        return result = 'You Win!'  
-    } else if ( computerChosen === 'paper' && userChosen === 'rock') {
-        return result = 'You Lost!'
-    } else if ( computerChosen === 'paper' && userChosen === 'scissors') {
+    } else if (computerChosen === 'rock' && userChosen === 'scissors') {
         return result = 'You Win!'
-    } else if ( computerChosen === 'scissors' && userChosen === 'rock') {
-        return result = 'You Win!'  
-    } else if ( computerChosen === 'scissors' && userChosen === 'paper') {
+    } else if (computerChosen === 'paper' && userChosen === 'rock') {
         return result = 'You Lost!'
-    }        
+    } else if (computerChosen === 'paper' && userChosen === 'scissors') {
+        return result = 'You Win!'
+    } else if (computerChosen === 'scissors' && userChosen === 'rock') {
+        return result = 'You Win!'
+    } else if (computerChosen === 'scissors' && userChosen === 'paper') {
+        return result = 'You Lost!'
+    }
+    
 }
+
+// collecting results
+
+// resultsList = []
